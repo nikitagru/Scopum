ade199777ac7e1a41216b8fafeef6d9784f67658
Nikita Grudinin <grunix123@gmail.com>
2020-10-09 00:46:24 +0500

    Доделан парсер JSON-файла блюд

--- src/main/java/Bot/JSONParse.java
+++ src/main/java/Bot/JSONParse.java
@@ -9,14 +9,79 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class JSONParse {
 
-    public HashMap<String, double[]> products = new HashMap<String, double[]>();
+    public Set products = new Set() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public Object[] toArray(Object[] objects) {
+            return new Object[0];
+        }
+
+        @Override
+        public boolean add(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection collection) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection collection) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection collection) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection collection) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+    };
     private JSONObject resultJson;
+
 
     public JSONObject productsInit(String path) throws ParseException {
         StringBuilder jsonStrBuilder = new StringBuilder();
@@ -46,19 +111,40 @@ public class JSONParse {
         return resultJson;
     }
 
-    public HashMap<String, double[]> convertJson() {
-        String result = resultJson.toJSONString();
+    public List<HashMap> convertJson() {
+        products = resultJson.entrySet();
+        List<Object> produc = new ArrayList<>();
 
-        String[] resultTable = result.split(":");
+        products.forEach(entry -> produc.add(entry));
 
-        System.out.println(resultTable);
-        return products;
+        List<HashMap> converteredProduct = new ArrayList<>();
+
+        for (int i = 0; i < produc.size(); i++) {
+            Object temp = produc.get(i);
+            temp = temp.toString();
+            converteredProduct.add(convertToMap((String) temp));
+        }
+
+        return converteredProduct;
     }
 
+    private HashMap<String, double[]> convertToMap(String product) {
+        String[] prodcutCalPFC = product.split("=");
+        prodcutCalPFC[1] = prodcutCalPFC[1].substring(1);
+        prodcutCalPFC[1] = prodcutCalPFC[1].replaceAll("}", "");
 
-//    public String findDailyProducts(double[] calPFC) {
-//
-//
-//
-//    }
+        String[] caloriesPFC = prodcutCalPFC[1].split(",");
+        double[] calPFC = new double[4];
+
+        for (int i = 0; i < 4; i++) {
+            caloriesPFC[i] = caloriesPFC[i].replaceAll("\"","");
+            String value = caloriesPFC[i].split(":")[1];
+            calPFC[i] = Double.parseDouble(value);
+        }
+
+        HashMap<String, double[]> result = new HashMap<>();
+        result.put(prodcutCalPFC[0], calPFC);
+
+        return result;
+    }
 }
--- src/main/java/Diet/ProductsFinder.java
+++ src/main/java/Diet/ProductsFinder.java
@@ -1,17 +1,22 @@
 package Diet;
 
 import Bot.JSONParse;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class ProductsFinder extends JSONParse {
 
     private JSONParse jsonObj = new JSONParse();
 
-    private JSONObject products = jsonObj.productsInit("e:\\JavaProjects\\Scopum\\Scopum\\src\\main\\java\\Diet\\Dishes.json");
+    private List<HashMap> productsCalPFC;
 
     public ProductsFinder() throws ParseException {
-        jsonObj.convertJson();
+        jsonObj.productsInit("e:\\JavaProjects\\Scopum\\Scopum\\src\\main\\java\\Diet\\Dishes.json");
+        productsCalPFC = jsonObj.convertJson();
     }
 
 
